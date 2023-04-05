c475962001108e7c38ce22cc31554898a7883f6c
1Doulingo <129946278+1Doulingo@users.noreply.github.com>
2023-04-05 19:24:36 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+
+function setup() {
+  createCanvas(400,400);
+  var sprite = createSprite (200,300,50,50)
+
+
+
+}
+
+
+
+
+
+function draw() 
+{
+  background(30);
+
+
+
+
+
+  drawSprites ()
+}
+
+
+
+
